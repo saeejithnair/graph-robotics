0bc90c38f1efbb3ef3d3d524c5b5d35f25680718
Saeejith Nair <saeejithn@gmail.com>
2024-08-21 13:06:09 -0400

    Got panda lift script working on guacamole. Instead of rendering to display, i render it to a video

--- robosuite/panda_lift.py
+++ robosuite/panda_lift.py
@@ -1,19 +1,66 @@
+import tyro
+from dataclasses import dataclass
+import imageio
 import numpy as np
-import robosuite as suite
+import robosuite.macros as macros
+from robosuite import make
+import os
 
-# create environment instance
-env = suite.make(
-    env_name="Lift", # try with other tasks like "Stack" and "Door"
-    robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
-    has_renderer=False,
-    has_offscreen_renderer=True,
-    use_camera_obs=False,
-)
+# Set the image convention to opencv
+macros.IMAGE_CONVENTION = "opencv"
 
-# reset the environment
-env.reset()
+# Set the MUJOCO_GL environment variable to use a software renderer
+os.environ["MUJOCO_GL"] = "osmesa"
 
-for i in range(1000):
-    action = np.random.randn(env.robots[0].dof) # sample random action
-    obs, reward, done, info = env.step(action)  # take action in the environment
-    env.render()  # render on display
+@dataclass
+class Args:
+    environment: str = "Lift"
+    robots: str = "Panda"
+    camera: str = "agentview"
+    video_path: str = "panda_lift_video.mp4"
+    timesteps: int = 500
+    height: int = 512
+    width: int = 512
+    skip_frame: int = 1
+
+def main(args: Args):
+    # Initialize environment with offscreen renderer
+    env = make(
+        args.environment,
+        robots=args.robots,
+        has_renderer=False,
+        has_offscreen_renderer=True,
+        ignore_done=True,
+        use_camera_obs=True,
+        use_object_obs=False,
+        camera_names=args.camera,
+        camera_heights=args.height,
+        camera_widths=args.width,
+    )
+
+    obs = env.reset()
+    ndim = env.action_dim
+
+    # Create a video writer with imageio
+    writer = imageio.get_writer(args.video_path, fps=20)
+
+    for i in range(args.timesteps):
+        # Run a uniformly random agent
+        action = np.random.randn(ndim)
+        obs, reward, done, info = env.step(action)
+
+        # Dump a frame from every K frames
+        if i % args.skip_frame == 0:
+            frame = obs[args.camera + "_image"]
+            writer.append_data(frame)
+            print(f"Saving frame #{i}")
+
+        if done:
+            break
+
+    env.close()
+    writer.close()
+    print(f"Video saved to {args.video_path}")
+
+if __name__ == "__main__":
+    tyro.cli(main)
